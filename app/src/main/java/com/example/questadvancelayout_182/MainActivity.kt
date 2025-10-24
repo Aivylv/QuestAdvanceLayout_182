dfe31af7e9557e1cc89f51b8f1fb667d5281f2fe
aivylv <lyvialarasati@gmail.com>
2025-10-24 10:32:10 +0700

    Menampilkan tampilan utama aplikasi dari fungsi ActivitasPertama menggantikan tampilan default Greeting

--- app/src/main/java/com/example/questadvancelayout_182/MainActivity.kt
+++ app/src/main/java/com/example/questadvancelayout_182/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAdvanceLayout_182Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Lyvia",
+                    ActivitasPertama(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
